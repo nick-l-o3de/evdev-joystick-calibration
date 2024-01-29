3525e4b0cb4f32f6dc8a1ddc494beced35eab4cb
Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>
2024-01-29 12:29:20 -0800

    Add heuristic for deadzone calibration.
    This is the first attempt, works for me for x360 and
    ps4 controllers.
    
    Signed-off-by: Nicholas Lawson <70027408+nick-l-o3de@users.noreply.github.com>

--- evdev_joystick_calibration/MinMaxItem.py
+++ evdev_joystick_calibration/MinMaxItem.py
@@ -6,9 +6,10 @@ class MinMaxItem:
         self.minimum = int(minimum)
         self.maximum = int(maximum)
         self.analog = analog
+        self.flat = 0 # flat aka deadzone
 
     def __str__(self):
-        return "analog: " + self.analog + " min:" + str(self.minimum) + " max:" + str(self.maximum)
+        return "analog: " + self.analog + " min:" + str(self.minimum) + " max:" + str(self.maximum) + " flat:" + str(self.flat)
 
 
 class MinMaxItemEncoder(json.JSONEncoder):
@@ -18,5 +19,5 @@ class MinMaxItemEncoder(json.JSONEncoder):
 
 def object_decoder(obj):
     if 'analog' in obj:
-        return MinMaxItem(obj['analog'], obj['minimum'], obj['maximum'])
+        return MinMaxItem(obj['analog'], obj['minimum'], obj['maximum'], obj['flat'])
     return obj
